7c125c8fcfc9ae191858b2c3c99382b1d59a2323
kndavenpo <122413804+kndavenpo@users.noreply.github.com>
2023-11-19 13:24:29 -0500

    Continue to setup project

---
+++ src/Login/index.js
@@ -0,0 +1,16 @@
+
+function Login() {
+  return(
+      <div>
+
+        <div className='page-padding'>
+
+          <h4 className="page-padding-top">LOGIN PAGE</h4>
+
+        </div>
+
+
+     </div>
+  );
+}
+export default Login;
