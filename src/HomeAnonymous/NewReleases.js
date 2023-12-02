8fed4355af9898fc7e935fc1be87b23c77bd8c7c
kndavenpo <122413804+kndavenpo@users.noreply.github.com>
2023-12-02 11:38:58 -0500

    Changes made on home and profile screens

---
+++ src/HomeAnonymous/NewReleases.js
@@ -0,0 +1,78 @@
+import * as client from "../client";
+import {useEffect, useState} from "react";
+
+
+function NewReleases(){
+
+// Get lists of fiction and nonfiction new releases
+  const [fictionReleases, setFictionReleases] = useState([]);
+  const [nonFictionReleases, setNonFictionReleases] = useState([]);
+
+  useEffect(() => {
+    const fetchLatestReleases = async () => {
+      try {
+        const searchTerm = 'new fiction releases'; // Adjust the search term for fiction
+        const fictionReleases = await client.findBooks(searchTerm);
+
+        const searchTermNonFiction = 'new nonfiction releases'; // Adjust the search term for non-fiction
+        const nonFictionReleases = await client.findBooks(searchTermNonFiction);
+
+        // Sort books based on publishedDate in descending order
+        const sortAndSlice = (books) =>
+            books
+                .sort((a, b) => new Date(b.volumeInfo.publishedDate) - new Date(a.volumeInfo.publishedDate))
+                .slice(0, 10);
+
+        setFictionReleases(sortAndSlice(fictionReleases));
+        setNonFictionReleases(sortAndSlice(nonFictionReleases));
+      } catch (error) {
+        console.error('Error fetching latest releases:', error);
+      }
+    };
+
+    fetchLatestReleases();
+  }, []);
+
+  const renderBookList = (books) => (
+      <div className="table-responsive">
+        <table className="table">
+          <tbody>
+          <tr>
+            {books.map((book) => (
+                <td key={book.id}>
+                  <div>
+                    {book.volumeInfo.imageLinks && (
+                        <img
+                            src={book.volumeInfo.imageLinks.smallThumbnail}
+                            alt={book.volumeInfo.title}
+                            className="img-thumbnail"
+                        />
+                    )}
+                  </div>
+                  <div>
+                    <p>
+                      <strong>{book.volumeInfo.title}</strong><br/>
+                      by {book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : 'Unknown Author'}</p>
+                  </div>
+                </td>
+            ))}
+          </tr>
+          </tbody>
+        </table>
+      </div>
+  );
+
+  return(
+
+      <div>
+        <h5>New Fiction Releases</h5>
+        {renderBookList(fictionReleases)}
+
+        <h5>New Non-Fiction Releases</h5>
+        {renderBookList(nonFictionReleases)}
+      </div>
+
+  )
+}
+
+export default NewReleases
--- src/HomeLoggedIn/index.js
+++ src/HomeLoggedIn/index.js
@@ -1,6 +1,5 @@
 import "../index.css";
-import BookList from "../HomeAnonymous/BookList";
-import Newsfeed from "./Newsfeed";
+import NewReleases from "../HomeAnonymous/NewReleases";
 import Navigation from "../Navigation";
 
 
@@ -8,16 +7,24 @@ function HomeLoggedIn() {
   return (
       <div>
         <Navigation />
-        <div className="row wd-general ">
-          <h5>Your Recommendations</h5>
-          <BookList />
-        </div><br/>
-        <div className="row wd-general">
-          <h5>Latest</h5>
+        <div className="wd-grid-col-wide-column wd-general">
+          <div className="row wd-general ">
+            <h5>PLACEHOLDER: Most Recent Additions to Your Bookshelf</h5><br/><br/>
+            <h5>ADD ANOTHER TABLE IN THE SAME FORMAT BELOW WITH RECENT BOOKS ADDED TO BOOKSHELF</h5><br/><br/>
+            <hr/>
+            <NewReleases />
+          </div>
+        </div >
+
+        <div className = "wd-grid-col-right-panel row wd-general">
+          <h5>
+            AUTHOR CORNER
+          </h5>
           <div>
-            <Newsfeed />
+            <p><strong>Author:</strong> Barbara Kingsolver</p>
           </div>
         </div>
+
       </div>
   );
 }
