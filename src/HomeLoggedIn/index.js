8e5fc9a105a7ec1691371bddae337e1cfd3fc41a
kndavenpo <122413804+kndavenpo@users.noreply.github.com>
2023-12-09 10:51:01 -0500

    work on Home-LoggedIn and profile

--- src/HomeLoggedIn/index.js
+++ src/HomeLoggedIn/index.js
@@ -1,30 +1,110 @@
 import "../index.css";
 import NewReleases from "../HomeAnonymous/NewReleases";
 import Navigation from "../Navigation";
-
+import * as client from "../users/client";
+import * as client2 from "../Profile/client";
+import { useState, useEffect } from "react";
 
 function HomeLoggedIn() {
+  const [profile, setProfile] = useState(null);
+  const [allBooks, setAllBooks] = useState([]);
+  const [booksWithPosts, setBooksWithPosts] = useState([]);
+
+  const fetchAccount = async () => {
+    const profile = await client.account();
+    setProfile(profile);
+  };
+  useEffect(() => {
+    fetchAccount();
+  }, []);
+
+  const fetchBooks = async () => {
+    if (profile) {
+      try {
+        const allBooksData = await client2.findAllBooks();
+        // Filter all books based on the current user's ID
+        const userBooks = allBooksData.filter(
+            (book) => book.user === profile._id
+        );
+        // Sort all books by dateAdded in descending order
+        const sortedAllBooks = [...userBooks].sort((a, b) =>
+            b.dateAdded.localeCompare(a.dateAdded)
+        );
+        // Limit the list to 10 books
+        const top10Books = sortedAllBooks.slice(0, 10);
+        setAllBooks(top10Books);
+
+
+        // Filter books with posts
+        const booksWithPostsData = top10Books.filter(
+            (book) => book.post && book.post.length > 0
+        );
+        setBooksWithPosts(booksWithPostsData);
+      } catch (error) {
+        console.error('Error fetching books:', error);
+      }
+    }
+  };
+
+
+
+  useEffect(() => {
+    fetchBooks();
+  }, [profile]);
+
+
   return (
       <div>
         <Navigation />
         <div className="wd-grid-col-wide-column wd-general">
           <div className="row wd-general ">
-            <h5>PLACEHOLDER: Most Recent Additions to Your Bookshelf</h5><br/><br/>
-            <h5>ADD ANOTHER TABLE IN THE SAME FORMAT BELOW WITH RECENT BOOKS ADDED TO BOOKSHELF</h5><br/><br/>
+            <h5>Most Recent Additions to Your Bookshelf</h5><br/><br/>
+            <div className="table-responsive">
+              <table className="table">
+                <tbody>
+                <tr>
+                  {allBooks && allBooks.map((book) => (
+                      <td key={book._id}>
+                        <div>
+                          {book.image && (
+                              <img
+                                  src={book.image}
+                                  className="img-thumbnail"
+                              />
+                          )}
+                        </div>
+                        <div>
+                          <p>
+                            <strong>{book.title}</strong><br/>
+                            by {book.author}</p>
+                        </div>
+                      </td>
+                  ))}
+                </tr>
+                </tbody>
+              </table>
+            </div>
             <hr/>
             <NewReleases />
           </div>
         </div >
-
-        <div className = "wd-grid-col-right-panel row wd-general">
-          <h5>
-            AUTHOR CORNER
-          </h5>
+        <div className="wd-grid-col-right-panel wd-general">
+          <h5>AUTHOR POSTS</h5><br/>
           <div>
-            <p><strong>Author:</strong> Barbara Kingsolver</p>
+            {booksWithPosts && (
+                <div>
+                  {booksWithPosts.map((book) => (
+                      <div key={book._id}>
+                        <p><strong>{book.author}</strong></p>
+                        <hr/>
+                        <p><strong>Bio</strong>: Add bio from users schema here. Try also to add posts from post schema rather than adding to books.</p>
+                        <p><strong>Post</strong>: {book.post}</p>
+                      </div>
+                  ))}
+                </div>
+            )}
           </div>
         </div>
-
       </div>
   );
 }
---
+++ src/Profile/client.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+const request = axios.create({
+  withCredentials: true,
+});
+
+export const BASE_API = "http://localhost:4000/api";
+//export const BASE_API = process.env.REACT_APP_API_BASE;
+export const BOOKS_API = `${BASE_API}/books`;
+
+// Books
+export const findAllBooks = async () => {
+  const response = await axios.get(`${BOOKS_API}`);
+  return response.data;
+};
+
+export const updateBook = async (book) => {
+  const response = await request.put(`${BOOKS_API}/${book._id}`, book);
+  return response.data;
+};
+
+
