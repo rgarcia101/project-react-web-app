ad8b39d0759eaf5aeda4564c43795fb3084dabb9
Steffi-Patel <122414146+Steffi-Patel@users.noreply.github.com>
2023-12-10 23:10:25 -0500

    bug fix for bar

--- src/HomeAnonymous/index.js
+++ src/HomeAnonymous/index.js
@@ -2,7 +2,7 @@ import "../index.css";
 import { BsBookFill } from "react-icons/bs";
 import NewReleases from "./NewReleases";
 import { useNavigate } from 'react-router-dom';
-
+import AnonymousNavigation from "./AnonymousNavigation";
 function HomeAnonymous() {
   const navigate = useNavigate();
 
@@ -24,15 +24,8 @@ function HomeAnonymous() {
   return (
       <div>
         {/*Top Navigation Bar */}
-        <div className="row gold-background wd-general">
-          <div>
-            <BsBookFill className="wd-home-icon" style={{ float: "left", fontSize: "2em", color: "black"}} />
-            <span style={{ fontSize: "1.5em"}}>BookBuddies</span>
-            <div className="float-end">
-            <button type="button" className="btn btn-warning" style={{ fontSize: "1.1em"}}onClick={handleSearch}>Search</button>
-              <button type="button" className="btn btn-warning" style={{ fontSize: "1.1em"}}onClick={handleSignin}>Sign In</button>
-              <button type="button" className="active-button" style={{ fontSize: "1.1em"}}onClick={handleGetStarted}>Get Started</button>
-            </div></div>
+        <div>
+          <AnonymousNavigation /> 
         </div>
 
         {/*Middle Navigation Bar*/}
